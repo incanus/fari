ee0eca33dc8526366ca429f973a286170a19f139
Justin R. Miller <incanus@codesorcery.net>
2020-05-11 20:51:52 -0700

    load, display, and open tabs

--- test.py
+++ test.py
@@ -2,20 +2,49 @@
 
 import argparse
 import curses
+import json
+import re
+import subprocess
 
-COUNT = 100
 SHOW_MAX = 10
+
+tabs = []
+
+def load_tabs():
+    global tabs
+    count = int(
+        subprocess.run([
+            'osascript',
+            '-e', 'tell app "Safari"',
+            '-e', '  get count of tabs of window 1',
+            '-e', 'end tell'
+        ], capture_output=True).stdout.decode('utf-8').strip()
+    )
+    details = json.loads(re.sub('},\s{', '],[', re.sub('}}$', ']]', re.sub('^{{', '[[',
+        subprocess.run([
+            'osascript', '-s', 's',
+            '-e', 'tell app "Safari"',
+            '-e', 'get {name, URL} of tabs of window 1',
+            '-e', 'end tell'
+        ], capture_output=True).stdout.decode('utf-8').strip()
+    ))))
+    for i in range(len(details[0])):
+        tabs.append({'name': details[0][i], 'url': details[1][i]})
 
 def main(s):
     if args.browse or True:
+        load_tabs()
         first = 0
         while True:
             s.clear()
-            show = SHOW_MAX if COUNT - first >= SHOW_MAX else COUNT - first + 1
+            count = len(tabs)
+            show = SHOW_MAX if count - first >= SHOW_MAX else count - first
             s.addstr(0, 0, f"Choose a tab "
-                           f"[{first}-{first + show - 1} of {COUNT}]:", curses.A_STANDOUT)
+                           f"[{first + 1}-{first + show} of {count}]:", curses.A_STANDOUT)
             for row in range(show):
-                s.addstr(row + 2, 0, f"{row}. foo")
+                label = tabs[first + row]['name'][:30].ljust(30)
+                url = re.sub('^https?://(www\.)?', '', tabs[first + row]['url'])[:20]
+                s.addstr(row + 2, 0, f"{row}. {label} ({url})")
             last = f"-{show - 1}" if show - 1 >= 1 else ""
             s.addstr(13, 0, f"[0{last}] to open, [<] or [>] to page, [q] to quit:")
             s.refresh()
@@ -24,9 +53,15 @@ def main(s):
                 first -= SHOW_MAX
                 first = 0 if first < 0 else first
             elif key == '>':
-                first = first + SHOW_MAX if first + SHOW_MAX <= COUNT else first
+                first = first + SHOW_MAX if first + SHOW_MAX <= count else first
             elif key == 'q':
                 break
+            else:
+                try:
+                    index = int(key)
+                    subprocess.run(['open', tabs[first + index]['url']])
+                except:
+                    pass
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-b", "--browse", action="store_true", help="browse tabs to open")
